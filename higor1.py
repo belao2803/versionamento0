8b6727e7c333be2c4b546093ed5f05ef06b06c17
isabelly <00001104099561sp@al.educaco.sp.gov.br>
2024-10-02 18:33:35 -0300

    versionamneto

--- higor1.py
+++ higor1.py
@@ -1,1 +1,13 @@
-print("Helo, word")
+candidatos = {
+    1 : {'nome:' "Luiz Inacio", 'partido:' "PT"},
+    2 : {'nome:' "Deodoro da Fonseca", 'partido:' "PRE"},
+    3 : {'nome:' "Prudente de Morais", 'partido:' "PL"},
+    4 : {'nome:' "Jair Bolsonaro", 'partido:' "PL"}
+}
+
+Governo = int(input("Digite o Numero do seu Candidato: "))
+
+if Governo in candidatos:
+    print(candidatos[Governo])
+else:
+    print("Candidato não encontrado.")
